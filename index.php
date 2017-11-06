52bfa804066dc55d74d8126648f63525f2d88991
Nachiket M Desai <nmd33@afsconnect1.njit.edu>
2017-11-05 23:08:07 -0500

    code editing

--- index.php
+++ index.php
@@ -22,8 +22,6 @@ try {
                 $statement->execute();
                 $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
                 $result = $statement->fetchAll();
-
-            
                 }
 
 catch (PDOException $e) {
@@ -32,22 +30,13 @@ catch (PDOException $e) {
 }
 echo '  Accounts table returned this records: are ' ;
 print_r(count($result));
-
 echo '<br>';
-
- 
-
-   echo '<table border="2">';
-
+   echo '<table border="3">';
  echo '<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>';
-
 foreach ($result as $row)
 
 {
-
     echo '<tr>';
-
-      
         echo '<td>'.$row['id'].'</td>';
         echo '<td>'.$row['email'].'</td>';
         echo '<td>'.$row['fname'].'</td>';
@@ -56,11 +45,7 @@ foreach ($result as $row)
         echo '<td>'.$row['birthday'].'</td>';
         echo '<td>'.$row['gender'].'</td>';
         echo '<td>'.$row['password'].'</td>';
-
     echo '</tr>';
-
 }
-
 echo '</table>';
-
 ?>
